2f6602b5b9b9a3aff674ce92c48ebe5f7590850a
erunions <78222901+erunions@users.noreply.github.com>
2022-04-19 13:36:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function createMap(position) {
+    const {longitude, latitude} = position.coords;
+
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        interactive: false,
+        center: [longitude, latitude],
+        antialias: true,
+        pitch: 40,
+        zoom: 16
+    })
+
+    const marker = new mapboxgl.Marker()
+    .setLngLat([longitude, latitude])
+    .addTo(map);
+}
+
+function errorHandler() {
+    console.log('Geolocation error');
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(createMap, errorHandler);
+} else {
+    console.log('Geolocation is not supported by your browser');
+}
+
+mapboxgl.accessToken = `pk.eyJ1IjoibGllY2h0ZW5zdGllbmZhbjIiLCJhIjoiY2wyNTRiajZqMW0wdjNpanJwYnN2dXQ5ZiJ9.Li9sZfg5_t0TzjBuhcUdRQ`;
